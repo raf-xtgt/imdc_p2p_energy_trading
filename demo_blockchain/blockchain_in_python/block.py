67b09cb26f20515128fc799d2186ce6d43d55691
Alam <md.rafaquatalam@gmail.com>
2022-01-12 20:52:20 +0800

    A simple blockchain in python

---
+++ demo_blockchain/blockchain_in_python/block.py
@@ -0,0 +1,29 @@
+#Reference: https://levelup.gitconnected.com/creating-a-blockchain-from-scratch-9a7b123e1f3e
+
+from hashlib import sha256
+from datetime import datetime
+
+class Block():
+  def __init__(self, data, previous_block_hash):
+    self.timestamp = datetime.utcnow()
+    self.data = data
+    self.prev_block_hash = previous_block_hash
+    # a valid hash will be calculated for every new block
+    self.calculate_valid_hash()
+
+  def is_hash_valid(self, hash):
+        return (hash.startswith('0' * 3))
+
+  def calculate_valid_hash(self):
+      hash = ''
+      nonce = 0
+
+      while (not self.is_hash_valid(hash)):
+          temp = self.to_string() + str(nonce)
+          hash = sha256(temp.encode()).hexdigest()
+          nonce += 1
+
+      self.hash = hash
+
+  def to_string(self):
+    return "{0}\t{1}\t{2}".format(self.data, self.timestamp, self.prev_block_hash)
