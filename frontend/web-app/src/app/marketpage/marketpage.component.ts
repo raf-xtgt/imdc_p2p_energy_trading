63e59ce50ca96ed7d8fb02ce8d778bbb536fdb32
Alam <md.rafaquatalam@gmail.com>
2022-01-30 20:56:11 +0800

    [Update] Market page for open requests

--- frontend/web-app/src/app/marketpage/marketpage.component.ts
+++ frontend/web-app/src/app/marketpage/marketpage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../config.service';
-
+import { BuyEnergyRequest } from '../classes';
 
 @Component({
   selector: 'app-marketpage',
@@ -11,6 +11,8 @@ export class MarketpageComponent implements OnInit {
 
   constructor(private _config:ConfigService) { }
 
+  public allBuyRequests:Array<BuyEnergyRequest>=[];
+
   ngOnInit(): void {
     this.getBuyRequests()
   }
@@ -18,7 +20,17 @@ export class MarketpageComponent implements OnInit {
 
   getBuyRequests(){
     this._config.getBuyRequests().subscribe(data => {
-      console.log("Buy requests data for market page", data)
+      //console.log("Buy requests data for market page", data)
+      let response = JSON.parse(JSON.stringify(data))
+      console.log("Buy requests data for market page", response)
+      //this.allBuyRequests = response.Requests
+      let reqArr = response.Requests
+      for(let i = 0; i < reqArr.length; i++) {
+        let request = new BuyEnergyRequest(reqArr[i].BuyerId, reqArr[i].EnergyAmount, reqArr[i].FiatAmount, reqArr[i].RequestClosed)
+        this.allBuyRequests.push(request)
+        // Prints i-th element of the array
+        //console.log(reqArr[i]);
+    }
     })
   }
 
