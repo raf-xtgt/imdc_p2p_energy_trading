29da0acf504d9faf85ec57e17d2f61ffbea7b777
Alam <md.rafaquatalam@gmail.com>
2022-02-07 13:30:51 +0800

    [Update] Python script connected to mongo db

--- backend/energy_Forcast/energyForecast.py
+++ backend/energy_Forcast/energyForecast.py
@@ -2,6 +2,11 @@ import numpy as np # linear algebra
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+import pymongo
+import os
+from dotenv import load_dotenv
+load_dotenv()
+PATH = '../src/.env'
 
 
 def energyForecast():
@@ -37,5 +42,23 @@ def energyForecast():
     plt.legend()
     plt.show()
 
+def connectToDb():
+    # load environment variables
+    db_username = os.getenv("DB_USERNAME")
+    db_password = os.getenv("DB_PASSWORD")
+    db_cluster = os.getenv("DB_CLUSTER_ADDR")
 
-energyForecast()
+    # connect to the database    
+    client = pymongo.MongoClient('mongodb+srv://'+db_username+':'+ db_password +'@imdc-p2p-energy.y0a68.mongodb.net/'+db_cluster+'?retryWrites=true&w=majority')
+    print("Connected to database")
+
+
+connectToDb()
+#energyForecast()
+
+
+"""
+1. Connect the python script to db.
+2. Find a way to call the python script from golang and pass in the user id as argument
+3. Create a forecast dataset for each user. [Data and user id]
+"""
