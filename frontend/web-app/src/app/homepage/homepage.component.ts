7b459d3a3480fe52201c989d6320e17355286c6c
Alam <md.rafaquatalam@gmail.com>
2022-02-19 12:21:27 +0800

    [Update] Topbar refresh issue fixed
    User does not need to repeatedly refresh the page to see their username and password

--- frontend/web-app/src/app/homepage/homepage.component.ts
+++ frontend/web-app/src/app/homepage/homepage.component.ts
@@ -10,13 +10,27 @@ import { ConfigService } from '../config.service';
 })
 export class HomepageComponent implements OnInit {
 
+  private isRefreshed :boolean = false;
   constructor(private _jwtServ:JWTService, private _config:ConfigService) { }
   
    ngOnInit(): void {
       this._jwtServ.verifyToken().subscribe(data => {
-        console.log("Verified Token", data)
+        if (data != null){
+          console.log("Verified Token", data)
+          this.refreshPage()
+        }
       })
-    
+  }
+
+  // refresh the page when there is no refreshed key in locals storage
+  refreshPage():void {
+    if (!localStorage.getItem('refreshed')) { 
+      localStorage.setItem('refreshed', 'no reload') 
+      location.reload() 
+    } else {
+      localStorage.removeItem('refreshed') 
+    }
+
   }
 
 }
